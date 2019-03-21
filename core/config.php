8bcde28e645d72a9350cb451f36fa47cad39d29c
bogdra <bogdan.dragoi@gmail.com>
2019-03-21 07:27:16 +0200

    added namespaces | added the core/Router.php

--- core/config.php
+++ core/config.php
@@ -4,3 +4,7 @@ namespace Core;
 
 //Site title
 define ('TITLE', 'OOP');
+
+
+define ('DEFAULT_CONTROLLER', 'Home');
+define ('DEFAULT_ACTION','Index');
--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <?php
+
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', __DIR__);
 
 include_once(ROOT. DS . 'core'. DS . 'config.php');
+//include_once(ROOT. DS . 'core'. DS . 'Router.php');
+
 
 function autoload($className)
 {
-    if (file_exists(ROOT. DS. 'intrefaces'. DS. $className)){
-        include_once(ROOT. DS. 'interfaces'. DS. $className);
-    } elseif (file_exists(ROOT. DS. 'core'. $className)) {
-        include_once(ROOT. DS. 'interfaces'. DS. $className);
-    } elseif (file_exists(ROOT. DS. 'app'. DS. 'controllers'. DS. $className)) {
-        include_once(ROOT. DS. 'app'. DS. 'controllers'. DS. $className);
-    } elseif (file_exists(ROOT. DS. 'app'. DS. 'models'. DS. $className)) {
-        include_once(ROOT. DS. 'app'. DS. 'models'. DS. $className);
+    $className = basename($className);
+    if (file_exists(ROOT. DS. 'intrefaces'. DS. $className. '.php')){
+        include_once(ROOT. DS. 'interfaces'. DS. $className. '.php');
+    } elseif (file_exists(ROOT. DS. 'core'. DS. $className. '.php')) {
+        include_once(ROOT. DS. 'core'. DS. $className. '.php');
+    } elseif (file_exists(ROOT. DS. 'app'. DS. 'controllers'. DS. $className. '.php')) {
+        include_once(ROOT. DS. 'app'. DS. 'controllers'. DS. $className. '.php');
+    } elseif (file_exists(ROOT. DS. 'app'. DS. 'models'. DS. $className. '.php')) {
+        include_once(ROOT. DS. 'app'. DS. 'models'. DS. $className. '.php');
     }
 
 }
@@ -22,4 +26,4 @@ spl_autoload_register('autoload');
 
 $url = isset( $_SERVER['PATH_INFO']) ? explode('/', trim($_SERVER['PATH_INFO'], '/')) : [];
 
-\Core\Router::route($url);
+$test = \Core\Router::route($url);
