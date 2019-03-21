bd139e495e77794f50467ec94cdb0c253c844600
bogdra <bogdan.dragoi@gmail.com>
2019-03-21 07:27:16 +0200

    added namespaces | added the core/Router.php

---
+++ core/Router.php
@@ -0,0 +1,26 @@
+<?php
+namespace Core;
+
+class Router
+{
+    public static function route(array $url): void
+    {
+
+      $controller = (isset($url[0]) && $url[0]!='') ? ucfirst($url[0]).'Controller' : DEFAULT_CONTROLLER.'Controller';
+      $action  = (isset($url[1]) && $url[1]!='') ? ucfirst($url[1]).'Action' : DEFAULT_ACTION.'Action';
+
+
+      //params
+       if (count($url) > 2) {
+           for ($i=1; $i<count($url);$i++) {
+               if ($url[$i] != '') {
+                   $params[] = $url[$i];
+               }
+           }
+       }
+
+        var_dump($controller.'/'.$action);
+    }
+}
+
+echo ('test');
--- index.php
+++ index.php
@@ -1,19 +1,24 @@
 <?php
+
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', __DIR__);
 
 include_once(ROOT. DS . 'core'. DS . 'config.php');
 
+
 function autoload($className)
 {
-    if (file_exists(ROOT. DS. 'intrefaces'. DS. $className)){
-        include_once(ROOT. DS. 'interfaces'. DS. $className);
-    } elseif (file_exists(ROOT. DS. 'core'. $className)) {
-        include_once(ROOT. DS. 'interfaces'. DS. $className);
-    } elseif (file_exists(ROOT. DS. 'app'. DS. 'controllers'. DS. $className)) {
-        include_once(ROOT. DS. 'app'. DS. 'controllers'. DS. $className);
-    } elseif (file_exists(ROOT. DS. 'app'. DS. 'models'. DS. $className)) {
-        include_once(ROOT. DS. 'app'. DS. 'models'. DS. $className);
+    $classNameWithNamespaceArray = explode('\\', $className);
+    $classFile = array_pop($classNameWithNamespaceArray).'.php';
+
+    if (file_exists(ROOT. DS. 'intrefaces'. DS. $classFile)){
+        include_once(ROOT. DS. 'interfaces'. DS. $classFile);
+    } elseif (file_exists(ROOT. DS. 'core'. DS. $classFile)) {
+        include_once(ROOT. DS. 'core'. DS. $classFile);
+    } elseif (file_exists(ROOT. DS. 'app'. DS. 'controllers'. DS. $classFile)) {
+        include_once(ROOT. DS. 'app'. DS. 'controllers'. DS. $classFile);
+    } elseif (file_exists(ROOT. DS. 'app'. DS. 'models'. DS. $classFile)) {
+        include_once(ROOT. DS. 'app'. DS. 'models'. DS. $classFile);
     }
 
 }
@@ -22,4 +27,4 @@ spl_autoload_register('autoload');
 
 $url = isset( $_SERVER['PATH_INFO']) ? explode('/', trim($_SERVER['PATH_INFO'], '/')) : [];
 
-\Core\Router::route($url);
+Core\Router::route($url);
