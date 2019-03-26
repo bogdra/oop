16ad66adde67be005faf132aab6ceda0d72ebafc
Bogdan Dragoi <bogdan.dragoi@gmail.com>
2019-03-26 11:06:18 +0200

    added the DB config constants

--- core/config.php
+++ core/config.php
@@ -7,6 +7,11 @@ define ('TITLE', 'OOP');
 
 define ('URL_ROOT', '/oop/');
 
+define ('DB_HOST', 'localhost');
+define ('DB_USER', 'root');
+define ('DB_PASS', '');
+define ('DB_NAME', 'oop');
+
 define ('DEFAULT_CONTROLLER', 'Home');
 define ('RESTRICTED_CONTROLLER', 'Restricted');
 define ('DEFAULT_ACTION', 'Index');
