45d7a393ce6635103ea5b13b0f98b21786ca0d49
bogdra <bogdan.dragoi@gmail.com>
2019-03-29 07:31:23 +0200

    fixed minor bug in H::dnd() - is string permanently present

--- core/H.php
+++ core/H.php
@@ -17,7 +17,10 @@ class H
     public static function dnl($variable, $variableName = '')
     {
         \echo('<pre style="border:1px solid #bbba74">');
-        \echo('<b>'.$variableName."</b> is  ");
+        if ($variableName != '')
+        {
+            \echo('<b>'.$variableName."</b> is  ");
+        }
         \print_r($variable);
         \echo('</pre>');
     }
