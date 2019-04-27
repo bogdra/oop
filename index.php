b33cb35f43f032be3ef5ca68aee2352f9e497811
bogdra <bogdan.dragoi@gmail.com>
2019-04-27 14:06:42 +0300

    added composer autoload

--- index.php
+++ index.php
@@ -1,30 +1,16 @@
 <?php
 
+
 use Core\Router;
+
 
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', __DIR__);
 
-include_once(ROOT. DS . 'core'. DS . 'config.php');
+require_once(ROOT . DS . 'vendor' . DS . 'autoload.php');
+include_once(ROOT . DS . 'core' . DS . 'config.php');
 
-function autoload($className)
-{
-    $classNameWithNamespaceArray = explode('\\', $className);
-    $classFile = array_pop($classNameWithNamespaceArray).'.php';
 
-    foreach(ROUTES as $route)
-    {
-        $route = ROOT .DS.str_replace('/', DS, $route). $classFile;
-        if (file_exists($route))
-        {
-            include_once($route);
-            break;
-        }
-    }
-}
-
-spl_autoload_register('autoload');
-
-$url = isset( $_SERVER['PATH_INFO']) ? explode('/', trim($_SERVER['PATH_INFO'], '/')) : [];
+$url = isset($_SERVER['PATH_INFO']) ? explode('/', trim($_SERVER['PATH_INFO'], '/')) : [];
 
 Router::route($url);
