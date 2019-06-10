949b429c59c99044dbd4c9fd44b5984eb89d5cbc
Bogdan Dragoi <bogdan.dragoi@gmail.com>
2019-06-10 16:21:32 +0300

    minor changes to index.php

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 
 
 use Core\Router;
-use App\Exceptions\HeadersAlreadySentException;
+use App\Exceptions\Request\HeadersAlreadySentException;
 
 
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', __DIR__);
 
-require_once(ROOT . DS . 'vendor' . DS . 'autoload.php');
 include_once(ROOT . DS . 'core' . DS . 'config.php');
+require_once(ROOT . DS . 'vendor' . DS . 'autoload.php');
 
 try {
     $url = isset($_SERVER['PATH_INFO']) ? explode('/', trim($_SERVER['PATH_INFO'], '/')) : [];
